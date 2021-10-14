366e91e52142abb337b1d3e3725aa3ee2c371ac9
KevinMons1 <kevin2001@hotmail.be>
2021-10-14 14:43:40 +0200

    try to turn around model

--- src/js/index.js
+++ src/js/index.js
@@ -6,10 +6,13 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 // Global varibale
 //------------------------
 
-let meshI = 0
+let scrollI = 0
 let initialPositionMeshY = 0.5
 let initialRotationMeshY = 0.25
-let groupObjectMemo = []
+
+let initialRotationGroupY = Math.PI * 0.75
+
+let scrollObjectI = []
 
 //------------------------
 // Base
@@ -50,11 +53,12 @@ window.addEventListener("resize", () => {
 
 // camera
 const camera = new THREE.PerspectiveCamera(75, sizesCanvas.width / sizesCanvas.height, 0.1, 100)
-camera.position.z = 5
+camera.position.z = 4.5
 scene.add(camera)
 
 // Controls
-// const controls = new OrbitControls(camera, canvas)
+const controls = new OrbitControls(camera, canvas)
+controls.enableZoom = false
 
 //------------------------
 // Model
@@ -90,33 +94,25 @@ const objectMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000 })
 // Create object and memo those positions
 for (let i = 0; i < 10; i++) {
     const object = new THREE.Mesh(objectGeometry, objectMaterial)
+    
+    // const positionZ = Math.sin(i) * 2.5
+    // const positionX = Math.cos(i) * 2.5
+    // const rotationY = Math.PI * Math.abs(positionZ, positionX)
+    
+    // object.position.x = positionX
+    // object.position.z = positionZ
+    // object.position.y = i - 10
 
-    const positionX = Math.cos(i) * Math.PI * 0.5
-    const positionZ = Math.sin(i) * Math.PI * 0.5
-    const positionY = i - 8
-    const rotationY = (initialRotationMeshY) * Math.PI * (- i)
+    // // object.rotation.y = - Math.abs(positionZ, positionX) * Math.PI * 0.5  // a garder car on se rapproche
+    // object.rotation.y = rotationY
 
-    object.position.x = positionX
-    object.position.z = positionZ
-    object.position.y = positionY
-    object.rotation.y = rotationY
-
-    // object.rotation.z = - 0.05
+    object.position.y = i - 13
 
     groupObject.add(object)
-
-    // Copy object for not reference
-    groupObjectMemo.push({
-        position: {
-            x: positionX,
-            z: positionZ,
-            y: positionY,
-        },
-        rotation: {
-            y: rotationY
-        }
-    })
+    scrollObjectI.push(0)
 }
+
+groupObject.rotation.y = initialRotationGroupY
 
 //------------------------
 // Renderer
@@ -136,46 +132,62 @@ window.addEventListener("wheel", e => {
     const speed = 0.005
 
     // Known down or up
-    if (e.deltaY < 0 && meshI > 0) meshI--
-    else if (e.deltaY > 0) meshI++
+    if (e.deltaY < 0 && scrollI > 0) scrollI--
+    else if (e.deltaY > 0) scrollI++
     
     //------
     // Update mesh
     //------
 
     // rotation
-    boxMesh.rotation.y = (initialRotationMeshY) - meshI * 0.01
+    boxMesh.rotation.y = (initialRotationMeshY) - scrollI * 0.01
 
     // position
-    boxMesh.position.y = (initialPositionMeshY) - meshI * speed
-    boxMesh.position.z = meshI * speed * 0.5
+    boxMesh.position.y = (initialPositionMeshY) - scrollI * (speed * 2.5)
+    boxMesh.position.z = scrollI * speed 
 
     //------
-    // Update element on group
+    // Update group of objects
     //------
-    
-    console.log(groupObjectMemo[0].position.x, Math.cos(meshI * 0.05))
-    groupObject.children.forEach((object, index) => {
-        // rotation
-        object.rotation.y = (groupObjectMemo[index].rotation.y) - meshI * 0.01 * 2
-        
-        // position
-        object.position.z = (groupObjectMemo[index].position.z) - Math.sin(meshI * 0.05) * Math.PI * 0.5 
-        object.position.x = (groupObjectMemo[index].position.x) - Math.cos(meshI * 0.05)
-        object.position.y = (groupObjectMemo[index].position.y) + (meshI * 0.05) 
+
+    // groupObject.rotation.y = (initialRotationGroupY) - (Math.sin(scrollI * 0.005) * Math.PI * 0.55) * 10
+    // groupObject.position.y = (scrollI * 0.08) 
+    groupObject.position.y = (scrollI * 0.04) 
+
+    groupObject.children.forEach((child, index) => {
+        console.log(scrollI)
+        if (groupObject.position.y >= 0) {
+            if (scrollI >= (index + 1) * 25) {
+                if (e.deltaY < 0 && scrollI > 0)  scrollObjectI[index]--
+                else if (e.deltaY > 0)  scrollObjectI[index]++
+
+                console.log(scrollObjectI[index])
+
+                const _index = groupObject.children.length - (index + 1)
+                groupObject.children[_index].position.z = Math.sin(scrollObjectI[index] * 0.05) * Math.PI * 0.6
+                groupObject.children[_index].position.x = Math.cos(scrollObjectI[index] * 0.05) * Math.PI * 0.6
+                groupObject.children[_index].rotation.y = Math.sin(scrollObjectI[index] * 0.007) * Math.PI * 0.5
+            }
+        }
+        // const _index = groupObject.children.length - (index + 1)
+        // groupObject.children[_index].position.z = Math.sin(scrollI * 0.05) * Math.PI * 0.6
+        // groupObject.children[_index].position.x = Math.cos(scrollI * 0.05) * Math.PI * 0.6
+        // groupObject.children[_index].rotation.y = Math.sin(scrollI * 0.007) * Math.PI * 0.5
     })
-    console.log((groupObjectMemo[0].position.x))
+
 })
 
-const animation = () => {
+
+
+const init = () => {
     // Update controls
-    // controls.update()
+    controls.update()
 
     // Update renderer
     renderer.render(scene, camera)
 
     // Call this function
-    window.requestAnimationFrame(animation)
+    window.requestAnimationFrame(init)
 }
 
-animation()
+init()
